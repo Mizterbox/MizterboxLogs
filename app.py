e33fef9ebc41c96b671a9292c0c0dff5f67b206e
logc <rakshithvbharani@gmail.com>
2019-02-24 01:44:39 -0800

    adding stuff

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from time import gmtime, strftime, localtime
-from dateutil import tz
+
 
 app = Flask(__name__)
 
@@ -15,20 +15,10 @@ def sprinklerlogs():
     content = request.get_json()
     sprinklerid = content['id']
     status = content['status']
-    from_zone = tz.getutil('UTC')
-    to_zone = tz.gettz('America/Los_Angeles')
-
-    utc_timestamp = strftime("%Y-%m-%d %H:%M:%S", localtime())
-
-    # Tell the datetime object that it's in UTC time zone since
-    # datetime objects are 'naive' by default
-    utc_timestamp = utc.replace(tzinfo=from_zone)
-
-    # Convert time zone
-    pacific_ts = utc.astimezone(to_zone)
+    timestamp = strftime("%Y-%m-%d %H:%M:%S", localtime())
     return jsonify(sprinklerid=sprinklerid,
     status=status,
-    timestamp = pacific_ts)
+    timestamp = timestamp)
 
 
 if __name__ == '__main__':
