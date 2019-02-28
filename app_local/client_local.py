261a9fed66bf3ca7f4261622f31600e9eed9e7f9
logc <rakshithvbharani@gmail.com>
2019-02-28 01:53:15 -0800

    adding logs

--- app_local/client_local.py
+++ app_local/client_local.py
@@ -7,12 +7,12 @@ status = np.random.randint(1000,size=100)
 maxcount = 5000000
 counter = 0
 
-actualsprinkids = [1,2,3,4,5]
+actualsprinkids = [1,2,4,0]
 status = ['Running Active', 'Restarting','Connecting to Wifi', 'Connecting to Internet']
 
 while counter < maxcount:
     res = requests.post('http://localhost:5000/sprinklerlogs/', json={
-    "id":int(np.random.choice(sprinklerid,size=1)[0])%len(actualsprinkids),
+    "id":int(np.random.choice(actualsprinkids,size=1)[0]),
     "status":np.random.choice(status,size=1)[0],
     })
 
--- app_local/routes.py
+++ app_local/routes.py
@@ -3,7 +3,16 @@ from app_local import db
 from app_local.utilities import capture_timestamp
 from flask import request, jsonify,render_template
 from app_local.models import MizterboxLogs
+from datetime import datetime
+from datetime import timedelta
+import time
 
+
+# time difference between now and the last log - in other words:
+# the maximum permissible time between now and registered device's last log
+# after which one can conclude the registered device under consideration is
+# down
+PERMITTED_TIME_BETWEEN_CHECKS = timedelta(seconds=45)
 
 @app.route("/")
 def hello():
@@ -13,7 +22,9 @@ def hello():
 @app.route("/sprinklerlogs/",methods=['GET','POST'])
 def sprinklerlogs():
     """
+    -------------------------------------------------------------------------------------
     Listens to JSON Logs from deployed esp8266 systems and stores them into the database.
+    -------------------------------------------------------------------------------------
     """
     # register the time as soon as the route was hit
     date = capture_timestamp()
@@ -34,3 +45,45 @@ def sprinklerlogs():
     return jsonify(sprinklerid=sprinklerid,
     status=status,
     timestamp=date)
+
+@app.route("/checkdowns/",methods=['GET'])
+def checkdowns():
+    """
+    ----------------------------------------------------------------------
+    Checks to see if any esp8266 devices are down.
+
+    Strategy: Check every registered device and see if time difference between now
+    and last log exceeds PERMITTED_TIME_BETWEEN_CHECKS.
+    ----------------------------------------------------------------------
+    """
+
+    registered_devices = [1,2,4,0]
+    down_ids = []
+
+    # this defines the new log windows as time uopdates
+    while True:
+        # from now, pull up all the records in the last hour.
+        last_hour_records = MizterboxLogs.query.filter(MizterboxLogs.timestamp>datetime.now()-timedelta(hours=0,minutes=60))
+
+        # iterate through the available registered devices.
+        for registered_device in registered_devices:
+
+            # get the most recent log of the registered_device -> latest log is obtained by picking the last element of the list.
+            latest_log = list(MizterboxLogs.query.filter(MizterboxLogs.sprinklerid==registered_device))[-1]
+
+            # if the time difference b/w now and the latest timestamp of the log exceeds 45 minutes (PERMITTED_TIME_BETWEEN_CHECKS)
+            # then the system-esp8266 is down.
+            current_time = datetime.now()
+            if current_time - latest_log.timestamp > PERMITTED_TIME_BETWEEN_CHECKS:
+                # add only if the esp8266 isn't present
+                if registered_device not in down_ids:
+                    down_ids.append(registered_device)
+
+            # if the esp8266 came back up then remove the registered_device from the down list.
+            if registered_device in down_ids:
+                if current_time - latest_log.timestamp < PERMITTED_TIME_BETWEEN_CHECKS:
+                    down_ids.remove(registered_device)
+
+            # delay for sometime
+            time.sleep(2)
+            print(down_ids)
