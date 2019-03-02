6ff1c27dc55154863d29d36d978abf9c733106e0
logc <rakshithvbharani@gmail.com>
2019-03-01 19:07:37 -0800

    updated proc

--- app_local/client.py
+++ app_local/client.py
@@ -4,16 +4,19 @@ from time import gmtime, strftime, localtime
 sprinklerid = np.random.randint(100,size=100)
 address = np.random.randint(1000,size=100)
 status = np.random.randint(1000,size=100)
+maxcount = 5000000
+counter = 0
 
-actualsprinkids = [1,2,3,4,5]
+actualsprinkids = [1,2,4,0]
 status = ['Running Active', 'Restarting','Connecting to Wifi', 'Connecting to Internet']
 
-while True:
-    res = requests.post('https://mizterboxlogs.herokuapp.com/sprinklerlogs/', json={
-    "id":int(np.random.choice(sprinklerid,size=1)[0])%len(actualsprinkids),
+while counter < maxcount:
+    res = requests.post('http://mizterboxlogs.herokuapp.com/sprinklerlogs/', json={
+    "id":int(np.random.choice(actualsprinkids,size=1)[0]),
     "status":np.random.choice(status,size=1)[0],
     })
 
     if res.ok:
         print (res.json())
     time.sleep(2)
+    counter+=1
