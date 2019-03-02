f6521e8b988df55f4e1f1e5e455c4ab0885bba25
logc <rakshithvbharani@gmail.com>
2019-03-01 20:54:36 -0800

    updated creds

--- app_local/__init__.py
+++ app_local/__init__.py
@@ -5,7 +5,7 @@ from pytz import timezone
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dbezkeuzwkxhnk:f0a41da23f5932a1f4a891b5e3b57c20b36f3b236614b74a8aa2e95e22f7c4d9@ec2-54-221-253-228.compute-1.amazonaws.com/postgresql-infinite-38158'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dbezkeuzwkxhnk:f0a41da23f5932a1f4a891b5e3b57c20b36f3b236614b74a8aa2e95e22f7c4d9@ec2-54-221-253-228.compute-1.amazonaws.com/d686176j9ebd4h'
 db = SQLAlchemy(app)
 
 
